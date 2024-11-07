0fdc4f93ecc4d1c80e0d0b4c07e6b1e87c8fc8d1
Andrew Van Valkenburgh <andrewvv@byu.edu>
2024-11-06 19:54:17 -0700

    Added javascript functionallity for the 3rd party api quote

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -2,12 +2,20 @@ import React from 'react';
 import './login.css';
 
 export function Login() {
+    const [quote, setQuote] = React.useState('Loading...');
+    const [quoteAuthor, setQuoteAuthor] = React.useState('unknown');
+
+    React.useEffect(() => {
+        setQuote('If you forced someone to do something they didn’t want to do, they’d just end up hating life.');
+        setQuoteAuthor('Chihiro Sengoku, Sakurasou no Pet na Kanojo');
+      }, []);
+
     return (
         <main className="container-fluid bg-secondary text-center">
             <div className="top-banner">
                 <h1>Welcome to Simply Cinema!</h1>
-                <h3>"If you forced someone to do something they didn’t want to do, they’d just end up hating life."</h3>
-                <h4>- Chihiro Sengoku, Sakurasou no Pet na Kanojo</h4>
+                <h3 className='quote'>{quote}</h3>
+                <h4 className='quoteAuthor'>{quoteAuthor}</h4>
             </div>
 
             <div className="picture-form">
