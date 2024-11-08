0d434d4d3eef6629abcf4a027585e7efa3b58bbb
Andrew Van Valkenburgh <andrewvv@byu.edu>
2024-11-08 13:57:36 -0700

    Working on adding functionallity to comments

--- src/comment/comment.jsx
+++ src/comment/comment.jsx
@@ -1,37 +1,85 @@
 import React from 'react';
 import './comment.css'
 import { useLocation } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 
 export function Comment() {
   const location = useLocation();
   const { mediaName = "No Media Selected" } = location.state || {};
 
+  const [description, setDescription] = React.useState("Add a description");
+  const [rating, setRating] = React.useState(0);
+  const [comments, setComments] = React.useState([]);
+
+  React.useEffect(() => {
+    const mediaText = localStorage.getItem(`${mediaName}`);
+    if (mediaText) {
+      const parsedMedia = JSON.parse(mediaText);
+      setComments(parsedMedia.comments);
+      setDescription(parsedMedia.description);
+      setRating(parsedMedia.rating);
+    }
+  }, []);
+
+  let commentRows = [];
+    if (comments.length) {
+      for (const [i, comment] of comments.entries()) {
+        commentRows.push(
+          <li className="list-group-item list-group-item-dark" key={i}>{comment}</li>
+        );
+      }
+    } else {
+      commentRows.push(
+        <li className="list-group-item list-group-item-dark" key={0}>Start adding comments below</li>
+      );
+    }
+
+  function updateMediaLocal() {
+    let prevMedia;
+    const newMedia = {
+      description: description,
+      rating: rating,
+      comments: comments
+    }
+    const mediaText = localStorage.getItem(`${mediaName}`);
+    if (mediaText) {
+      prevMedia = JSON.parse(mediaText);
+    }
+
+    if (newMedia !== newMedia) {
+      localStorage.setItem(`${mediaName}`, JSON.stringify(newMedia));
+    }
+  }
+
+  const [descVal, setDescVal] = React.useState("");
+  const updateDescription = () => {
+    setDescription(descVal);
+    updateMediaLocal();
+  }
+  const logDescription = event => {
+      setDescVal(event.target.value);
+  }
+
   return (
     <main className="container-fluid bg-secondary text-center">
       <div className="title">
         <h2>{mediaName}</h2>
-        <h4>Rating: 4.5/5</h4>
+        <h4>Rating: {rating}/5</h4>
       </div>
 
       <div className="main-body">
         <div className="description">
           <h4 className="cd-header">Description:</h4>
-          <p>Star Wars is a multimedia space opera that combines science fiction and fantasy elements to create a universe with a variety of stories</p>
-          <textarea id="description" name="description" rows="5" cols="25" placeholder="Write New Description"></textarea>
+          <p>{description}</p>
+          <textarea id="description" name="description" rows="5" cols="25" placeholder="Write New Description" onChange={logDescription} value={descVal}></textarea>
           <div className="description-submit">
-            <form action="comment.html" method="get">
-              <button type="submit" className="btn btn-primary">Submit New Description</button>
-            </form>
+            <Button type="submit" className="btn btn-primary" onClick={updateDescription}>Submit New Description</Button>
           </div>
         </div>
 
         <div className="comments">
           <h4 className="cd-header">Comments:</h4>
-          <ul className="list-group">
-            <li className="list-group-item list-group-item-dark">It's great!</li>
-            <li className="list-group-item list-group-item-dark">It's kinda overrated...</li>
-            <li className="list-group-item list-group-item-dark">Luke is so cool!</li>
-          </ul>
+          <ul className="list-group">{commentRows}</ul>
           <form action="comment.html" method="get">
             <input className="form-control" type="text" placeholder="Comment Here"></input>
             <button type="submit" className="btn btn-primary">Submit New Comment</button>
