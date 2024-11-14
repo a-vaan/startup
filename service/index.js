fd12bcd001122b34ed611e8cc879e4a777756212
Andrew Van Valkenburgh <andrewvv@byu.edu>
2024-11-13 17:36:17 -0700

    Added GetMediaList and AddMedia endpoints

--- service/index.js
+++ service/index.js
@@ -57,15 +57,15 @@ apiRouter.delete('/auth/logout', (req, res) => {
   res.status(204).end();
 });
 
-// GetScores
-apiRouter.get('/scores', (_req, res) => {
-  res.send(scores);
+// GetMediaList
+apiRouter.get('/media', (_req, res) => {
+  res.send(mediaList);
 });
 
-// SubmitScore
-apiRouter.post('/score', (req, res) => {
-  scores = updateScores(req.body, scores);
-  res.send(scores);
+// AddMedia
+apiRouter.post('/add-media', (req, res) => {
+  mediaList.push(req.body);
+  res.send(mediaList);
 });
 
 // Return the application's default page if the path is unknown
@@ -76,25 +76,3 @@ app.use((_req, res) => {
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
-
-// updateScores considers a new score for inclusion in the high scores.
-function updateScores(newScore, scores) {
-  let found = false;
-  for (const [i, prevScore] of scores.entries()) {
-    if (newScore.score > prevScore.score) {
-      scores.splice(i, 0, newScore);
-      found = true;
-      break;
-    }
-  }
-
-  if (!found) {
-    scores.push(newScore);
-  }
-
-  if (scores.length > 10) {
-    scores.length = 10;
-  }
-
-  return scores;
-}
