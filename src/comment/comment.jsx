ba537f4889c65261671f9b9ddbb78916e5267fc1
Andrew Van Valkenburgh <andrewvv@byu.edu>
2024-12-03 17:04:32 -0700

    Modifying websocket receiving code

--- src/comment/comment.jsx
+++ src/comment/comment.jsx
@@ -20,17 +20,12 @@ export function Comment() {
       let port = window.location.port;
       const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
       this.socket = new WebSocket(`${protocol}://${window.location.hostname}:${port}/ws`);
-      this.socket.onopen = (event) => {
-        this.receiveEvent(new EventMessage('Comments', { msg: 'connected' }));
-      };
-      this.socket.onclose = (event) => {
-        this.receiveEvent(new EventMessage('Comments', { msg: 'disconnected' }));
-      };
       this.socket.onmessage = async (msg) => {
-        try {
-          const event = JSON.parse(await msg.data.text());
-          this.receiveEvent(event);
-        } catch {}
+        await fetch(`/api/comments/${mediaId}`)
+          .then((response) => response.json())
+          .then((comments) => {
+            setComments(comments);
+          })
       };
     }
   
